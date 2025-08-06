2e7e88ba7af0b2db00e5ee4e7980c22adea9e46f
Madhu Akula <madhu.akula@hotmail.com>
2025-08-06 16:46:56 -0700

    added footer

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -23,6 +23,9 @@ export default defineConfig({
       customCss: [
         './src/styles/custom.css',
       ],
+      components: {
+        Footer: './src/components/Footer.astro',
+      },
       sidebar: [
         {
           label: 'Getting Started',
